5a62bed16888ad9d6cb623c53e338a0b95689e7d
MuzaffarTusunov96 <Muzaffar.Tursunov102196@gmail.com>
2023-01-06 23:19:38 +0300

    ss

--- race/urls.py
+++ race/urls.py
@@ -28,7 +28,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('imgapi.urls')),
-    path('login/' , LoginView.as_view(template_name='login.html')),
+    path('accounts/login/' , LoginView.as_view(template_name='login.html')),
     
     path('swagger/', schema_view.with_ui('swagger',
                                          cache_timeout=0), name='schema-swagger-ui'),
