fb103c7a8d0af05bac14e83c7f2e3f37e1c4ce4b
MuzaffarTusunov96 <Muzaffar.Tursunov102196@gmail.com>
2023-01-07 00:38:13 +0300

    sdd

--- imgapi/urls.py
+++ imgapi/urls.py
@@ -6,11 +6,11 @@ from . import views
 
 
 urlpatterns = [
-    path('image-add', views.UserImageApi.as_view(), name='a'),
-    path('image-list', views.UserImageListApi.as_view(), name='image-list'),
-    path('image-delete/<pk>', views.ImageDelete.as_view(), name='image-delete'),
-    path('image-detail/<pk>', views.UserImageDetail.as_view(), name='image-detail'),
-    path('image-filter/<name>',views.UserImageFilterByName.as_view(), name='image-filter'),
+    path('image-add', views.UserImageApi.as_view(), name='image_add'),
+    path('image-list', views.UserImageListApi.as_view(), name='image_list'),
+    path('image-delete/<pk>', views.ImageDelete.as_view(), name='image_delete'),
+    path('image-detail/<pk>', views.UserImageDetail.as_view(), name='image_detail'),
+    path('image-filter/<name>',views.UserImageFilterByName.as_view(), name='image_filter'),
 
 
 ]
